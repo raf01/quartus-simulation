ebc7f89e1bd5748549c1f4959d81b9e0dbb20efb
Rafael Pontes <rafaelgpontes1@gmail.com>
2016-11-24 19:51:57 -0300

    Projeto certo

--- mux_2x1.sv
+++ mux_2x1.sv
@@ -1,13 +1,13 @@
-`timescale 1ns/1ps
-
 module mux_2x1(
-	input logic in0, in1, sel,
-	output logic out);
 
-always_comb
-	if(sel == 'h0)
-		out <= in0;
-	else
-		out <= in1;
+   input logic in0, in1, sel,
+   output logic out);
+
+   always_comb
+      if (sel == 'h0)
+         out <= in0;
+      else
+         out <= in1;
 
 endmodule
+
--- tb.sv
+++ tb.sv
@@ -1,28 +1,41 @@
+`timescale 10ns/10ps
+
 module tb();
 
-logic data0, data1;
-logic [1:0]sel;
-logic mux_out;
+   logic data0, data1;
+   logic [1:0] sel;
+   logic mux_out;
 
-mux_2x1 mux_test (.in0(data0), .in(data1), .sel(sel), .out(mux_out));
+   mux_2x1 mux_i(.in0(data0),
+                 .in1(data1),
+                 .sel(sel),
+                 .out(mux_out));
 
-initial
-begin
-	data0 = 'h0;
-	data1 = 'h0;
-end
+   initial begin
+      data0 = 'h0;
+      data1 = 'h0;
+   end
 
-always
-	#5 data0 <= ~data0;
-	
-always
-	#20 data1 <= ~data1;
-	
-initial begin
-	sel = 'b0;
-	#100
-	
-	sel = 'b1;
-	#100
+   always
+      #5ns data0 = ~data0;
+
+   always
+      #20ns data1 = ~data1;
+
+   initial begin
+      //Select data0 and wait #100
+      sel = 'b0;
+      #100ns;
+
+      //Select data1 and wait #100
+      sel = 'b1;
+      #100ns;
+		
+		$finish();
+   end
+
+   initial begin
+     $dumpfile ("waves.vcd");
+     $dumpvars (1, tb);
+   end
 endmodule
-	
