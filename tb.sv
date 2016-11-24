fcba16876c95c9f2cb5670f9efc3b14534c404d0
Rafael Pontes <rafaelgpontes1@gmail.com>
2016-11-24 18:44:24 -0300

    Implementação da aula.

--- tb.sv
+++ tb.sv
@@ -1,13 +1,15 @@
 module tb();
 
-logic data0, data1, sel, mux_out;
+logic data0, data1;
+logic [1:0]sel;
+logic mux_out;
 
 mux_2x1 mux_test (.in0(data0), .in(data1), .sel(sel), .out(mux_out));
 
 initial
 begin
 	data0 = 'h0;
-	data1 = 'h1;
+	data1 = 'h0;
 end
 
 always
@@ -16,5 +18,11 @@ always
 always
 	#20 data1 <= ~data1;
 	
+initial begin
+	sel = 'b0;
+	#100
+	
+	sel = 'b1;
+	#100
 endmodule
 	
